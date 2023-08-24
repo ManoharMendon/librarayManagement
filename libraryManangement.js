b27ea55fdd0e000af6623099fe7dd61755b9d0af
ManoharMendon <manumendon98@gmail.com>
2023-08-24 21:06:28 +0530

    first commit

---
+++ libraryManangement.js
@@ -0,0 +1,70 @@
+
+    function Book(title, author) {
+            this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+      }
+    // var Book = {
+    //   title = "title",
+    //   author = "author",
+    //   isAvailable = "true",
+    // }
+
+
+    const library = [];
+
+    function addBook(title, author) {
+        const newBook = {
+          title: title,
+          author: author,
+          isAvailable: true
+        };
+        library.push(newBook);
+      }
+      
+      
+      function borrowBook(title) {
+        for (let i = 0; i < library.length; i++) {
+          if (library[i].title === title && library[i].isAvailable) {
+            library[i].isAvailable = false;
+            return true; 
+          }
+        }
+        return false; 
+      }
+      
+     
+      function returnBook(title) {
+        for (let i = 0; i < library.length; i++) {
+          if (library[i].title === title && !library[i].isAvailable) {
+            library[i].isAvailable = true;
+            return true; 
+          }
+        }
+        return false; 
+      }
+      
+      
+      function listBooks() {
+        console.log("Library Catalog:");
+        for (const book of library) {
+          console.log(`Title: ${book.title}`);
+          console.log(`Author: ${book.author}`);
+          console.log(`Available: ${book.isAvailable ? "Yes" : "No"}`);
+          console.log("---------------------------");
+        }
+      }
+      
+addBook("The Hobbit", "J.R.R. Tolkien");
+addBook("Harry Potter and the Sorcerer's Stone", "J.K. Rowling");
+borrowBook("The Hobbit");
+listBooks();
+returnBook("The Hobbit");
+listBooks();
+      
+      
+      
+      
+      
+      
+      
